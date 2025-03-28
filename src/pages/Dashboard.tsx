55778c00c5a34cb151ee198a4318ac7700c9c76c
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-03-28 22:02:09 +0000

    feat: adds  logout

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,16 @@
-import { TrendingUp } from "lucide-react";
+import { LogOut, TrendingUp } from "lucide-react";
+import { useAuthStore } from "../store/auth";
+import { useNavigate } from 'react-router-dom'
 
 export function Dashboard() {
+    const navigate = useNavigate();
+    const { logout } = useAuthStore();
+
+    const handleLogout = () => {
+        logout();
+        navigate('/login');
+    }
+
     return(
         <div className="min-h-screen bg-dark p-6">
             <div className="max-w-7xl mx-auto">
@@ -9,8 +19,26 @@ export function Dashboard() {
                         <TrendingUp className="text-primary"/>
                         Painel Financeiro
                     </h1>
+                    <button 
+                        onClick={handleLogout}
+                        className="flext items-center gap-2 bg-red-500/10 text-red-400 py-2 px-4 rounded-lg hover:bg-red-500/20 transition-colors"
+                    >
+                        <LogOut size={20}/>
+                        Sair
+                    </button>
+                </div>
+
+                <div className="grid md-grid-colors-2 gap-6">
+                    <div className="bg-dark-card rounded-lg shadow-xl border border-gray-800 p-6">
+                        <h2 className="text-xl font-semibold mb-6 text-gray-100">Visão Geral do Mercado</h2>
+                        <div className="space-y-4">
+                            
+                        </div>
+                    </div>
                 </div>
             </div>
+
+            
         </div>
     )
 }
